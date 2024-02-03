f7df3d932367b066214c971abf95d6c90187b68c
cAhyoung <joayoung03@gmail.com>
2024-02-03 22:48:07 +0900

    240203 백준 28278번

---
+++ Baekjoon/data_structure/stack2_28278.py
@@ -0,0 +1,28 @@
+n = int(input())
+stack = []
+for i in range(n):
+    order = list(map(int, input().split()))
+
+    if order[0] == 1:
+        stack.append(order[1])
+
+    elif order[0] == 2:
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+
+    elif order[0] == 3:
+        print(len(stack))
+
+    elif order[0] == 4:
+        if stack:
+            print(0)
+        else:
+            print(1)
+
+    else:
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
